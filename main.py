8ff11bbb5cd6c0472eed5cc62748e3b27fad888e
Erik Schahine <Erik.schahine@live.se>
2023-07-13 21:57:06 +0200

    Added move_balls function,interpolation needs to be added.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import random
 window = tk.Tk()
 
 frame = tk.Frame(window)
@@ -10,14 +11,22 @@ canvas.pack()
 Simbutton = tk.Button(frame, text="Start Simulation")
 Simbutton.pack()
 
-
+balls = []
 def create_ball(event):
     x = event.x
     y = event.y
-    canvas.create_oval(x-10, y-10, x+10, y+10, fill="blue")  
-canvas.bind("<Button-1>", create_ball)  
+    ball = canvas.create_oval(x-10, y-10, x+10, y+10, fill="blue")
+    balls.append(ball) 
+canvas.bind("<Button-1>", create_ball)
 
-#button.bind("", lambda event: func())
+def move_balls():
+    for ball in balls:
+        dx = random.randint(-3,3)
+        dy = random.randint(-3,3)
+        canvas.move(ball,dx,dy)
+    window.after(100, move_balls)
+#use interpolation to create smooth movement..
+move_balls()
 
 window.mainloop()
 
