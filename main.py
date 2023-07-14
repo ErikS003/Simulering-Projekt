40d9992117a66c37c6c51cff7686e1bba82ce5e2
Lisa Sandin <lisa1.sandin@gmail.com>
2023-07-14 19:53:27 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
 import tkinter as tk
 import random
-window = tk.Tk()
 
-frame = tk.Frame(window)
+window = tk.Tk()    #creates window
+
+frame = tk.Frame(window)    #creates frame
 frame.pack()
 
-canvas = tk.Canvas(frame, width=400, height=300)
+canvas = tk.Canvas(frame, width=400, height=300)    #creates canvas
 canvas.pack()
 
-Simbutton = tk.Button(frame, text="Start Simulation")
+Simbutton = tk.Button(frame, text="Start Simulation")   #creates button for starting simulation
 Simbutton.pack()
 
 balls = []
-def create_ball(event):
-    x = event.x
-    y = event.y
-    ball = canvas.create_oval(x-10, y-10, x+10, y+10, fill="blue")
-    balls.append(ball) 
-canvas.bind("<Button-1>", create_ball)
+def create_ball(event):     #event is the coordinate of where button 1 is activated
+    x = event.x             #x-coordinate
+    y = event.y             #y-coordinate
+    ball = canvas.create_oval(x-10, y-10, x+10, y+10, fill="blue")      #Creates the ball
+    balls.append(ball)      #put the ball in a list
+canvas.bind("<Button-1>", create_ball)    #When button 1 is activated. Create_ball function is run.
 
 def move_balls():
-    for ball in balls:
-        dx = random.randint(-3,3)
-        dy = random.randint(-3,3)
-        canvas.move(ball,dx,dy)
-    window.after(100, move_balls)
+    for ball in balls:      #goes through every ball created
+        dx = random.randint(-3,3)       #Creates a random x-coordinate
+        dy = random.randint(-3,3)       #Creates a random y-coordinate
+        canvas.move(ball,dx,dy)         #Moves the ball dx amount in x direction and dy amount in y direction.
+    canvas.after(100, move_balls)       #After 100 ms. Run move_balls again.
 #use interpolation to create smooth movement..
+
+#Simbutton.bind("", lambda event: move_balls())
 move_balls()
 
 window.mainloop()
